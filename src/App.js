868badd5a60f8b8f4804bf7d01a4b7ece5c3c67b
crisjur <69446539+crisjur@users.noreply.github.com>
2021-03-05 00:40:55 +0800

    push!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 
 import './App.css';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi, Simple Business
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
